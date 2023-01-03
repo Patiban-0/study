72114ef138a4e26065072e5b3715ceef8f08c7e5
Patiban-0 <121844527+Patiban-0@users.noreply.github.com>
2023-01-03 14:06:37 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,29 @@
-function a(b2){
-  console.log(b2);
+//変数
+let a1;
+const b1;
+var c1;
+//関数
+function a(bb){
+  console.log(bb)
 }
 a('(ﾟ∀ﾟ)ｱﾋｬ');
+aa(bbb)=>{
+  console.log(bbb)
+}
+aa('(ﾟ∀ﾟ)ｱﾋｬ2');
+//if文
+let 1=0;
+if(1=0){
+  console.log('〇');
+}else{
+  console.log('✕');
+}
+//繰り返し文(while)
+let o=0;
+while(o=0){
+  console.log(o);
+  if(o=0){
+    console.log(o+'Test');
+  }
+  a++;
+}
