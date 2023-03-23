d7b7b6cb04ee2255c35e7c99f59677d18ec838a2
Ptbnth <121844527+Ptbnth@users.noreply.github.com>
2023-03-23 11:14:18 +0000

    main

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-(async()=>{
-  const sha256 = async (text) => {
-    const uint8  = new TextEncoder().encode(text)
-    const digest = await crypto.subtle.digest('SHA-256', uint8)
-    return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2,'0')).join('')
+const sha256 = async (text) => {
+  const uint8  = new TextEncoder().encode(text)
+  const digest = await crypto.subtle.digest('SHA-256', uint8)
+  return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2,'0')).join('')
 }
-  let pass = prompt('Enter Password');
-  if(await sha256(pass) == "0c87ed818fb90f3f88faa6b362cf1e99025f3c1248f8fe513cdb38d03d9dce65"){
-    alert('success');
-    console.log('success');
+function getCookieValue(key) {
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+      var cookiesArray = cookie.split('='); 
+      if (cookiesArray[0].trim() == key.trim()) { 
+          return cookiesArray[1];
+      }
+  }
+  return '';
+}
+let sold_nic_person_spaceAD_bwi = document.body.innerHTML;
+document.body.innerHTML = "<br><br><br><br><br><br><b>Enter Password</b><br>";
+let input = document.createElement("input");
+input.id = "pass";
+input.type = "text";
+document.body.appendChild(input);
+let button = document.createElement("button");
+button.innerHTML = "OK";
+button.onclick = async () => {
+  if (await sha256(document.getElementById("pass").value) == "230661e0b854a75267ed8a5a7e410bb9b1a4256a18171c9b7122f07d8a75ac8c"){
+    document.body.innerHTML = sold_nic_person_spaceAD_bwi;
+    document.cookie = "sold = success;path = /;domain = ptbnth.github.io/study;max-age = 9999999999";
+    location.reload();
   }else{
-    alert('誰だお前');
-    console.log('誰だお前');
-    location.href="https://google.com";
+    console.error("パスワードが違います");
+    alert("パスワードが違います");
+  }
+}
+  document.body.appendChild(button);
+  if(getCookieValue("sold") == "success"){
+    document.body.innerHTML = sold_nic_person_spaceAD_bwi;
   }
